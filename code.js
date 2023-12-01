8e4c9d258788a71399d388c21daedf9e7feae446
agustin <agussosa@hotmail.es>
2023-12-01 13:06:58 -0300

    cambio de api y ult arreglos

--- code.js
+++ code.js
@@ -1,26 +1,40 @@
-
-const OPTIONS = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '917bb1a01amshd4d0dc3e22275d3p1ad0edjsne59b83d54963',
-		'X-RapidAPI-Host': 'ip-forensics-ip-geolocation-currency-exchange-and-threat-intelligence-api.p.rapidapi.com'
-	}
-};
-
-  function fetchIpInfo(ip) {
-     fetch(`https://ip-forensics-ip-geolocation-currency-exchange-and-threat-intelligence-api.p.rapidapi.com/single?apikey=917bb1a01amshd4d0dc3e22275d3p1ad0edjsne59b83d54963&ip_address=${ip}`, OPTIONS)
-    .then(res => res.json())
-    .catch(err => console.error(err))
-  }
-
-
-
-  const $ = (selector) => document.querySelector(selector)
+const $ = (selector) => document.querySelector(selector)
 
   const $form = $('#form')
   const $input = $('#input')
   const $submit = $('#submit')
   const $results = $('#results')
+
+
+const OPTIONS = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '917bb1a01amshd4d0dc3e22275d3p1ad0edjsne59b83d54963',
+		'X-RapidAPI-Host': 'ip-geolocation-ipwhois-io.p.rapidapi.com'
+	}
+};
+
+
+async function fetchIpInfo(ip) {
+    try {
+        const respuesta = await fetch(`http://ipwho.is/${ip}`, OPTIONS);
+        if (!respuesta.ok) {
+            throw new Error(`Error al cargar los datos desde ${ip}`);
+        }
+
+        const datos = await respuesta.json();
+
+        if ($results) {
+            console.log(datos)
+            return $results.innerHTML = JSON.stringify(datos, null, 2);
+            
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
 
   $form.addEventListener('submit', async (e)=>{
     e.preventDefault()
@@ -30,15 +44,10 @@ const OPTIONS = {
     $submit.setAttribute('disabled', '')
     $submit.setAttribute('aria-busy', 'true')
 
-    const ipInfo = await fetchIpInfo(value)
-    
+    await fetchIpInfo(value)
 
-    if(ipInfo){
-        $results.innerHTML = JSON.stringify(ipInfo, null, 2)
-    }
-
-    $submit.removeAttribute('disable', '')
-    $submit.removeAttribute('aria-busy', 'true')
+    $submit.removeAttribute('disabled'); // Change here
+    $submit.removeAttribute('aria-busy'); // Change here
 
   })
 
